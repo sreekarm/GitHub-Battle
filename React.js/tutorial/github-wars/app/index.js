e43b09c1599a58e296d892e3bb136804e8b5d127
Sreekar Maganti <maganti.sreekar@gmail.com>
2019-09-26 14:07:00 +0530

    Cleaned up directories

---
+++ React.js/tutorial/github-wars/app/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+//Component is concerned with
+//--State
+//--Lifecycle
+//--UI
+
+class App extends React.Component {
+    render() {
+        return(
+            <div>
+                First draft!
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    //React Element
+    <App />
+    //Where to render this element
+    document.getElementById('app')
+)
