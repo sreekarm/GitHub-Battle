161f8c27aea4bc99ced405d250eada7720ac305e
Sreekar Maganti <maganti.sreekar@gmail.com>
2019-09-28 17:05:20 +0530

    Created battle results skeleton

--- React.js/tutorial/github-wars/app/components/Battle.js
+++ React.js/tutorial/github-wars/app/components/Battle.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle} from 'react-icons/fa'
+import Results from './Results'
 
 function Instructions(){
     return(
@@ -119,7 +120,8 @@ export default class Battle extends React.Component{
 
         this.state={
             playerOne: null,
-            playerTwo: null
+            playerTwo: null,
+            battle: false
         }
 
         this.handleSubmit = this.handleSubmit.bind(this)
@@ -137,6 +139,10 @@ export default class Battle extends React.Component{
     }
     render(){
         const{playerOne, playerTwo} = this.state
+
+        if(battle === true){
+            return <Results playerOne={playerOne} playerTwo={playerTwo} />
+        }
         
         return(
             <React.Fragment>
@@ -168,6 +174,14 @@ export default class Battle extends React.Component{
                                 />
                         }
                     </div>
+
+                {playerOne && playerTwo && (
+                    <button
+                    className='btn btn-dark btn-space'
+                    onClick={() => this.setState({battle: true})}>
+                        Battle
+                    </button>
+                )}    
                 </div>
 
             </React.Fragment>
