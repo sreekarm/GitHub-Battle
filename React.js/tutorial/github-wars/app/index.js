99cb925f07b80006d8166403ae948b72228acf1e
Sreekar Maganti <maganti.sreekar@gmail.com>
2019-10-02 11:52:59 +0530

    Created React Context method Theme Provider

--- React.js/tutorial/github-wars/app/index.js
+++ React.js/tutorial/github-wars/app/index.js
@@ -3,17 +3,32 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import Popular from './components/Popular'
 import Battle from './components/Battle'
+import ThemeProvider from '../contexts/theme'
 //Component is concerned with
 //--State
 //--Lifecycle
 //--UI
 
 class App extends React.Component {
+    constructor(){
+        super(props)
+
+        this.state = {
+            theme: 'light',
+            toggleTheme: () => {
+                this.setState(({theme}) => ({
+                    theme: theme === 'light' ? 'dark' : 'light'
+                }))
+            } 
+        }
+    }
     render() {
         return(
-            <div className = 'container'>
-                <Battle />
-            </div>
+            <ThemeProvider value={this.state}>
+                <div className = 'container'>
+                    <Battle />
+                </div>
+            </ThemeProvider>
         )
     }
 }
