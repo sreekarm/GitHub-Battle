b1751e095fa8eaa3432340ef74bf7b665d3ed655
Sreekar Maganti <maganti.sreekar@gmail.com>
2019-10-03 12:42:24 +0530

    Added Navbar

--- React.js/tutorial/github-wars/app/components/Nav.js
+++ React.js/tutorial/github-wars/app/components/Nav.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import {ThemeConsumer} from '../contexts/theme'
+import {NavLink} from 'react-router-dom'
+
+const activeStyle = {
+    color: 'rgb(189, 47, 29)'
+}
 
 export default function Nav(){
     return(
         <ThemeConsumer>
             {({theme, toggleTheme}) => (
                 <nav className = 'row space-between'>
+                    <ul className='row nav'>
+                        <li>
+                            <NavLink to='/' exact activeStyle={activeStyle} className='nav-link'> Popular </Link>
+                        </li>
+                        <li>
+                            <NavLink to='/battle' activeStyle={activeStyle} className='nav-link'> Battle </Link>
+                        </li>
+                    </ul>
                     <button
                         style={{fontSize: 30}}
                         className='btn-clear'
